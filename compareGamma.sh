ac3c906f6e5620972b397eabfa0d096a79591145
Karol Krizka <kkrizka@gmail.com>
2014-03-31 18:13:42 -0500

    Made compareGamma and compareJet scripts to compare gamma-based samples and jet-based samples. compare_stack can now select directory which to compare.

---
+++ compareGamma.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ${#} != 1 ]; then
+    echo "usage: ${0} tag"
+    exit -1
+fi
+
+tag=${1}
+
+
+#./compare_stack.py root/${tag}.Egamma.event.root:style=data root/${tag}.Pythia8_gammajet.event.root:style=pythia8-gammajet root/${tag}.Herwigpp_gammajet.event.root:style=herwigpp-gammajet-n 'plot/*' -s styles/gammajet.style -o ${tag}.root
+./compare_stack.py root/${tag}.Egamma.event.root:style=data root/${tag}.Herwigpp_gammajet.event.root:style=herwigpp-gammajet -d plot -s styles/gammajet.style -o ${tag}.root
+
+./hsplot.py ${tag}.root --logy $--ratio=Data --ratiorange=0:2 --nostack
--- compare_stack.py
+++ compare_stack.py
@@ -10,11 +10,12 @@ parser.add_argument('input', metavar='INPUT', nargs='+', help='Input ROOT files 
 parser.add_argument('-o','--output', metavar='OUTPUT',default=None,help='ROOT file where the output will be stored.')
 parser.add_argument('-s','--style', metavar='STYLE',default=None,help='Style to apply to the histograms.')
 parser.add_argument('-n','--name', metavar='NAME',default=None,help='Plot only histograms matching this.')
+parser.add_argument('-d','--dir', metavar='DIR',default=None,help='TDirectory where to start the search.')
 
 args = parser.parse_args()
 
 o=TFile.Open(args.output,'RECREATE') if args.output!=None else None
-s=style.Style(args.style) if args.style!=None else None
+s=style.Style(args.style)
 name_match=args.name
 
 def get_hists(f,prefix=[]):
@@ -42,11 +43,13 @@ for input in args.input:
         extrainfo[key]=value
     
     f=TFile.Open(input)
+    d=f if args.dir==None else f.Get(args.dir)
 
-    hists=get_hists(f)
+    hists=get_hists(d)
     for histname in hists:
-        if not fnmatch.fnmatch(histname,name_match): continue
-        hist=f.Get(histname)
+        if name_match!=None and not fnmatch.fnmatch(histname,name_match): continue
+        print histname
+        hist=d.Get(histname)
         histname=histname.replace('/','_')
         if histname not in hstacks:
             hstack=THStack()
@@ -56,7 +59,8 @@ for input in args.input:
             hstack.SetTitle(';%s;'%xtitle)
 
             hstacks[histname]=hstack
-        if 'style' in extrainfo: s.apply_style(extrainfo['style'],hist)
+
+        if s!=None: s.apply_style(hist,extrainfo)
         hstacks[histname].Add(hist,hist.GetOption())            
         
     fs.append(f)
@@ -71,8 +75,8 @@ for name,hstack in hstacks.items():
     hstack.Write()
     c.SaveAs('%s.pdf'%name)
 
+if o!=None: o.Close()
+
 for f in fs:
     f.Close()
 
-if o!=None: o.ls()    
-if o!=None: o.Close()
