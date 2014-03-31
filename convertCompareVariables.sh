782f2ea4fffa2f6bf77181abc720dc144f7f44e5
Karol Krizka <kkrizka@gmail.com>
2014-03-31 18:14:31 -0500

    Added Nataliia's tagging to convert script.

--- convertCompareVariables.sh
+++ convertCompareVariables.sh
@@ -8,10 +8,20 @@ fi
 
 input=${1}
 inputname=$(basename ${1})
+iden=$(echo ${inputname} | cut -f 1 -d .)
+
+export PYTHONPATH=$(pwd):${PYTHONPATH}
+
+if [ ! -d ${iden} ]; then
+    mkdir ${iden}
+fi
 
 for i in $(seq 0 2); do
-    ./sorted_stack.py ${input} -s styles/qgtag.style -b 2 -o plot${i}_${inputname} -d plot${i}
+    ./sorted_stack.py ${input} -s styles/qgtag.style -b 2 -o ${iden}/plot${i}_${inputname} -d plot${i}
+    ./sorted_stack.py ${input} -s styles/qgtag.style -b 2 -o ${iden}/naplot${i}_${inputname} -d naplot${i}
+    ./sorted_stack.py ${input} -s styles/qgtag.style -b 0 -o ${iden}/plotunbin${i}_${inputname} -d plotunbin${i}
 
-    ./plotvspt.py plot${i}_${inputname} plot${i}_vspt_${inputname}
-    ./plotvspt-fractions.py plot${i}_${inputname} plot${i}_fractions_${inputname}
+    ./plotvspt.py ${iden}/plot${i}_${inputname} ${iden}/plot${i}_vspt_${inputname}
+    ./plotvspt-fractions.py ${iden}/plot${i}_${inputname} ${iden}/plot${i}_fractions_${inputname}
+    ./plotvspt-fractions.py ${iden}/naplot${i}_${inputname} ${iden}/naplot${i}_fractions_${inputname}
 done
